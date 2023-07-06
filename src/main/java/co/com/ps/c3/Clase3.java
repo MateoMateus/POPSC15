5bd713a983172ee9999972c8945f3e1224cab527
Mateo Mateus <matius.1512@gmail.com>
2023-07-05 19:04:37 -0500

    git add .

--- src/main/java/co/com/ps/c3/Clase3.java
+++ src/main/java/co/com/ps/c3/Clase3.java
@@ -2,24 +2,20 @@ package co.com.ps.c3;
 
 import java.util.Scanner;
 
-public class Clase3 {
+//public class Clase3 {
+//
+  //  public static void main(String[] args) {
+    //    Scanner scanner = new Scanner (System.in);
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+      //  System.out.println("Digite su nombre: ");
+        //String nombre =scanner.nextLine();
 
-        System.out.println("Digite su nombre: ");
-        String nombre =scanner.nextLine();
+        //System.out.println("Digite su edad: ");
+        //int edad = scanner.nextInt();
 
-        System.out.println("Digite su edad: ");
-        int edad = scanner.nextInt();
+        //System.out.println(" Su nombre es "+ nombre + " ,Su edad es " + edad +  " años  .Bienvenido ");
 
-        System.out.println(" Su nombre es "+ nombre + " ,Su edad es " + edad +  " años  .Bienvenido ");
 
-        Persona persona = new Persona();
-        persona.setEdad(13);
-        persona.setNombre("mateo");
 
-        persona.getEdad();
 
-     }
-}
+
